9cc9ef3539df291703e321da6ece6d1480688873
jareklojewski <jarek@orbitera.com>
2016-03-21 16:15:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>PHP Test -deployed by Code deploy</title>
  </head>
  <body>
 echo "<body style='background-color:green'>";
- <?php echo '<p>This is Jarek page - color green</p>'; ?> 
+ <?php echo '<p>This is Jarek page - color green - code deployed</p>'; ?> 
  </body>
 </html>
